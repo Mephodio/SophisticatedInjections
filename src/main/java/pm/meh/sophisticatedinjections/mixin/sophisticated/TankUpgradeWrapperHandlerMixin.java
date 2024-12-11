6e87aaa5ae8fe68e4ceccd80ced6588bc66b2910
Mephody <mephodio@gmail.com>
2024-12-11 22:14:21 +0300

    Input slot accepts just potions

--- src/main/java/pm/meh/sophisticatedinjections/mixin/sophisticated/TankUpgradeWrapperHandlerMixin.java
+++ src/main/java/pm/meh/sophisticatedinjections/mixin/sophisticated/TankUpgradeWrapperHandlerMixin.java
@@ -1,8 +1,7 @@
 package pm.meh.sophisticatedinjections.mixin.sophisticated;
 
-import com.simibubi.create.content.fluids.transfer.GenericItemEmptying;
+import com.simibubi.create.content.fluids.potion.PotionFluidHandler;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.server.ServerLifecycleHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class TankUpgradeWrapperHandlerMixin {
     @Inject(method = "isValidInputItem(Lnet/minecraft/world/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true, remap = false)
     public void injectIsValidFluidItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (GenericItemEmptying.canItemBeEmptied(ServerLifecycleHooks.getCurrentServer().overworld(), stack)) {
+        if (PotionFluidHandler.isPotionItem(stack)) {
             cir.setReturnValue(true);
             cir.cancel();
         }
