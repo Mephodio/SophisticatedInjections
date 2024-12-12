a5548d64c1e703cd20d5b4a98b80dffb15da448a
Mephody <mephodio@gmail.com>
2024-12-13 01:14:23 +0300

    Use `toggle upgrade` keybind to inject potion

---
+++ src/main/java/pm/meh/sophisticatedinjections/mixin/sophisticated/UpgradeToggleMessageMixin.java
@@ -0,0 +1,29 @@
+package pm.meh.sophisticatedinjections.mixin.sophisticated;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.IBackpackWrapper;
+import net.p3pp3rf1y.sophisticatedbackpacks.network.UpgradeToggleMessage;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeWrapper;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import pm.meh.sophisticatedinjections.upgrades.injection.InjectionUpgradeWrapper;
+
+import java.util.Map;
+
+@Mixin(UpgradeToggleMessage.class)
+@Debug(export = true)
+public class UpgradeToggleMessageMixin {
+    @Inject(method = "lambda$handleMessage$1(Lnet/p3pp3rf1y/sophisticatedbackpacks/network/UpgradeToggleMessage;Lnet/minecraft/server/level/ServerPlayer;Lnet/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/IBackpackWrapper;)V",
+            at = @At(value = "INVOKE", target = "Lnet/p3pp3rf1y/sophisticatedcore/upgrades/IUpgradeWrapper;setEnabled(Z)V"),
+            locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
+    private static void injectSetEnabled(UpgradeToggleMessage msg, ServerPlayer player, IBackpackWrapper w, CallbackInfo ci, Map slotWrappers, IUpgradeWrapper upgradeWrapper) {
+        if (upgradeWrapper instanceof InjectionUpgradeWrapper injectionWrapper) {
+            injectionWrapper.injectIntoPlayer(player);
+        }
+    }
+}
--- src/main/java/pm/meh/sophisticatedinjections/upgrades/injection/InjectionUpgradeWrapper.java
+++ src/main/java/pm/meh/sophisticatedinjections/upgrades/injection/InjectionUpgradeWrapper.java
@@ -1,13 +1,57 @@
 package pm.meh.sophisticatedinjections.upgrades.injection;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeWrapperBase;
+import pm.meh.sophisticatedinjections.util.PotionHelper;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class InjectionUpgradeWrapper extends UpgradeWrapperBase<InjectionUpgradeWrapper, InjectionUpgradeItem> {
     protected InjectionUpgradeWrapper(IStorageWrapper backpackWrapper, ItemStack upgrade, Consumer<ItemStack> upgradeSaveHandler) {
         super(backpackWrapper, upgrade, upgradeSaveHandler);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+//        super.setEnabled(enabled);
+    }
+
+    public void injectIntoPlayer(Player player) {
+        storageWrapper.getFluidHandler().ifPresent(fluidHandler -> {
+            int portionSize = PotionHelper.getBottleAmount();
+
+            for (int tank = 0; tank < fluidHandler.getTanks(); tank++) {
+                FluidStack fluidInTank = fluidHandler.getFluidInTank(tank);
+
+                if (fluidInTank.getAmount() >= portionSize) {
+                    CompoundTag fluidTag = fluidInTank.getOrCreateTag();
+                    List<MobEffectInstance> mobEffects = PotionUtils.getAllEffects(fluidTag);
+
+                    if (!mobEffects.isEmpty()) {
+                        fluidHandler.drain(new FluidStack(fluidInTank, portionSize), IFluidHandler.FluidAction.EXECUTE, false);
+
+                        for (MobEffectInstance effectInstance : mobEffects) {
+                            MobEffect effect = effectInstance.getEffect();
+                            if (effect.isInstantenous()) {
+                                effect.applyInstantenousEffect(null, null, player, effectInstance.getAmplifier(), 0.5D);
+                            } else {
+                                player.addEffect(new MobEffectInstance(effectInstance));
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+        });
     }
 }
