9d870e09261f96a5f9c48ed9707b0600e4decbaf
Mephody <mephodio@gmail.com>
2024-12-13 21:10:00 +0300

    Add delay before injection

--- src/main/java/pm/meh/sophisticatedinjections/upgrades/injection/InjectionUpgradeWrapper.java
+++ src/main/java/pm/meh/sophisticatedinjections/upgrades/injection/InjectionUpgradeWrapper.java
@@ -1,26 +1,36 @@
 package pm.meh.sophisticatedinjections.upgrades.injection;
 
 import com.simibubi.create.AllSoundEvents;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.ITickableUpgrade;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeWrapperBase;
+import org.jetbrains.annotations.Nullable;
 import pm.meh.sophisticatedinjections.util.PotionHelper;
 
 import java.util.List;
 import java.util.function.Consumer;
 
-public class InjectionUpgradeWrapper extends UpgradeWrapperBase<InjectionUpgradeWrapper, InjectionUpgradeItem> {
+public class InjectionUpgradeWrapper extends UpgradeWrapperBase<InjectionUpgradeWrapper, InjectionUpgradeItem>
+        implements ITickableUpgrade {
+
+    private static final int INJECTION_DELAY = 20;
+
+    long scheduledInjectionTime = 0;
+    List<MobEffectInstance> scheduledEffects = null;
+
     protected InjectionUpgradeWrapper(IStorageWrapper backpackWrapper, ItemStack upgrade, Consumer<ItemStack> upgradeSaveHandler) {
         super(backpackWrapper, upgrade, upgradeSaveHandler);
     }
@@ -30,7 +40,32 @@ public class InjectionUpgradeWrapper extends UpgradeWrapperBase<InjectionUpgrade
         return false;
     }
 
+    @Override
+    public void tick(@Nullable LivingEntity entity, Level level, BlockPos blockPos) {
+        if (scheduledInjectionTime > 0 && scheduledInjectionTime <= level.getGameTime()) {
+            for (MobEffectInstance effectInstance : scheduledEffects) {
+                MobEffect effect = effectInstance.getEffect();
+                if (effect.isInstantenous()) {
+                    effect.applyInstantenousEffect(null, null, entity, effectInstance.getAmplifier(), 0.5D);
+                } else {
+                    entity.addEffect(new MobEffectInstance(effectInstance));
+                }
+            }
+
+            entity.level().playSound(null, entity.getX(), entity.getY(), entity.getZ(),
+                    AllSoundEvents.STEAM.getMainEvent(), SoundSource.PLAYERS, 1.0f, 0.8f);
+
+            scheduledInjectionTime = 0;
+            scheduledEffects = null;
+        }
+    }
+
     public void injectIntoPlayer(Player player) {
+
+        if (scheduledInjectionTime > 0) {
+            return;
+        }
+
         storageWrapper.getFluidHandler().ifPresent(fluidHandler -> {
             int portionSize = PotionHelper.getBottleAmount();
             boolean injected = false;
@@ -40,34 +75,22 @@ public class InjectionUpgradeWrapper extends UpgradeWrapperBase<InjectionUpgrade
 
                 if (fluidInTank.getAmount() >= portionSize) {
                     CompoundTag fluidTag = fluidInTank.getOrCreateTag();
-                    List<MobEffectInstance> mobEffects = PotionUtils.getAllEffects(fluidTag);
+                    scheduledEffects = PotionUtils.getAllEffects(fluidTag);
 
-                    if (!mobEffects.isEmpty()) {
+                    if (!scheduledEffects.isEmpty()) {
                         fluidHandler.drain(new FluidStack(fluidInTank, portionSize), IFluidHandler.FluidAction.EXECUTE, false);
-
-                        for (MobEffectInstance effectInstance : mobEffects) {
-                            MobEffect effect = effectInstance.getEffect();
-                            if (effect.isInstantenous()) {
-                                effect.applyInstantenousEffect(null, null, player, effectInstance.getAmplifier(), 0.5D);
-                            } else {
-                                player.addEffect(new MobEffectInstance(effectInstance));
-                            }
-                        }
                         injected = true;
+                        scheduledInjectionTime = player.level().getGameTime() + INJECTION_DELAY;
                         break;
                     }
                 }
             }
 
-            if (injected) {
-                player.displayClientMessage(Component.literal("Injected potion"), true);
-                player.level().playSound(null, player.getX(), player.getY(), player.getZ(),
-                        AllSoundEvents.STEAM.getMainEvent(), SoundSource.PLAYERS, 1.0f, 0.8f);
-            } else {
-                player.displayClientMessage(Component.literal("No potions available"), true);
-                player.level().playSound(null, player.getX(), player.getY(), player.getZ(),
-                        AllSoundEvents.SPOUTING.getMainEvent(), SoundSource.PLAYERS, 1.0f, 2.0f);
-            }
+            String msg = injected ? "Injecting potion" : "No potions available";
+            player.displayClientMessage(Component.literal(msg), true);
+
+            player.level().playSound(null, player.getX(), player.getY(), player.getZ(),
+                    AllSoundEvents.SPOUTING.getMainEvent(), SoundSource.PLAYERS, 1.0f, 2.0f);
         });
     }
 }
